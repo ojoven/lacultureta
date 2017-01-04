b7e385f7f9946537dd463075d9dc71d63a1cc887
ojoven <mikeltorresmail@gmail.com>
2017-01-04 18:53:04 +0100

    Initial commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+// Models
+
+class IndexController extends Controller {
+
+    public function playground() {
+
+        // Code to play with here
+
+
+        return view('playground');
+
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hoy No Hay Nada</title>
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            AQUÍ VA TODO<br>
+            <a href="/playground">Playground</a>
+        </div>
+    </body>
+</html>
