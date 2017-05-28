2ad1e4ff8e0e65e57bedfae31ecbca6d669db9e6
ojoven <mikeltorresmail@gmail.com>
2017-05-28 19:32:21 +0200

    Create base InitialLoadTest

---
+++ tests/Feature/InitialLoadTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class InitialLoadTest extends TestCase {
+
+    public function testInitialLoadIsAValidJson() {
+
+    }
+
+}
