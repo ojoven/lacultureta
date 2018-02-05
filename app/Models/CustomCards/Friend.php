87054a3431bda6b7694c21e0c86c4e09ec96b944
ojoven <mikeltorresmail@gmail.com>
2018-02-05 12:26:48 +0100

    Add The Mêlée Fusión to Friends

--- app/Models/CustomCards/Friend.php
+++ app/Models/CustomCards/Friend.php
@@ -27,6 +27,14 @@ class Friend extends Model {
 				$card['url'] = 'http://themelee.org/post/167481575436/the-m%C3%AAl%C3%A9e-making-of-con-noelia-lozano';
 				$card['urlText'] = "¡Haz click aquí<br>si quieres saber más!";
 				break;
+
+			case 'TheMeleeFusion':
+				$card['title'] = __('¡Encuentro de diseñadoras y programadoras* en The Mêlée!');
+				$card['image'] = '/img/friends/themeleefusion.jpg';
+				$card['description'] = __('Este viernes 9 en la sala Keler ven a conocer a la comunidad The Mêlée y disfruta con la charlas de Ricardo Félix y Diego Rodríguez.<br><br><span style="font-size:11px;line-height: 1;">* Personas. Mujeres y hombres bienvenidas en una comunidad de aspiración diversa.</span>'); // @codingStandardsIgnoreLine
+				$card['url'] = 'http://themelee.org/post/169964774746/the-m%C3%AAl%C3%A9e-dise%C3%B1o-y-desarrollo-fusi%C3%B3n';
+				$card['urlText'] = "¡Anímate y haz click aquí!";
+				break;
 		}
 
 		if ($card) {
